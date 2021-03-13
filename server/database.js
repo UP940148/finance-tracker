18d3ba4705481bd3b59d4c502dd9df649b475b75
UP940148 <up940148@myport.ac.uk>
2021-03-13 16:37:12 +0000

    Added CREATE statements for user and transactions tables

--- server/database.js
+++ server/database.js
@@ -7,7 +7,37 @@ database.open(config.DBSOURCE)
   .then(_db => {
     db = _db;
     // Connection established. Create tables
-    
+    console.log('Connection to SQLite database has been established!');
+    db.run(`CREATE TABLE IF NOT EXISTS user (
+      googleId PRIMARY KEY NOT NULL,
+      name TEXT NOT NULL,
+      email TEXT UNIQUE NOT NULL
+    );`)
+      .then(() => {
+        // Table established
+        console.log('Established user table');
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+
+    db.run(`CREATE TABLE IF NOT EXISTS transactions (
+      userId references user(googleId) NOT NULL,
+      date INTEGER,
+      amount REAL,
+      memo TEXT,
+      address TEXT,
+      payee TEXT,
+      category TEXT,
+      subcategory TEXT
+    );`)
+      .then(() => {
+        // Table established
+        console.log('Established transactions table');
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
   })
   .catch(err => {
     // Database failed to open
