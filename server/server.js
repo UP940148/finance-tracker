d7eff33295ba1fa338941b12cab03bcca8b71438
UP940148 <up940148@myport.ac.uk>
2021-03-18 19:27:09 +0000

    Added error handling for stay-awake module if not found

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const db = require('./database.js');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const stayAwake = require('stay-awake');
 const googleAuth = require('simple-google-openid');
 const auth = googleAuth(config.OAUTH_CLIENT_ID);
 const multer = require('multer');
@@ -22,12 +21,19 @@ const uploader = multer({
 
 const jsonParser = bodyParser.json();
 
-stayAwake.prevent(function(err, data) {
-  console.log(`${data} routines are preventing sleep!\nYour computer will stay awake whilst this process is running\n`);
-});
+// This just keeps your computer from sleeping whilst web server is running
+try {
+  const stayAwake = require('stay-awake');
+  stayAwake.prevent(function(err, data) {
+    console.log(`${data} routines are preventing sleep!\nYour computer will stay awake whilst this process is running\n`);
+  });
+} catch {
+  console.log("non-essential development module not found: 'stay-awake'\nSkipping\n");
+}
 
 app.use('/', express.static(config.www, {index: 'welcomePage.html', extension: ['HTML'] }));
 
+// Set up web server on port specified in config file. Default to port 8080 if there's an error with config.PORT value
 app.listen(config.PORT || 8080, (err) => {
   console.log(`Server runnning on port ${config.PORT || 8080}!`);
 });
